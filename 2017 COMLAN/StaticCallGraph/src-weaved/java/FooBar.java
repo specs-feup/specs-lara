e49cfd79790a5c56c7ea1e014e7baf7a280687f8
JoaoBispo <joaobispo@gmail.com>
2017-07-26 01:54:34 +0100

    [COMLAN] Adds StaticCallGraph example for all weavers

---
+++ 2017 COMLAN/StaticCallGraph/src-weaved/java/FooBar.java
@@ -0,0 +1,21 @@
+
+
+
+public class FooBar {
+    public double bar() {
+        return 1.0;
+    }
+
+    public double foo() {
+        double a = 0;
+        for (int i = 0; i < 5; i++) {
+            a += bar();
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        new FooBar().foo();
+    }
+}
+
---
+++ 2017 COMLAN/StaticCallGraph/src-weaved/matlab/foobar.m
@@ -0,0 +1,15 @@
+function foobar()
+   foo();
+end
+
+function [a] = foo()
+   a = 0;
+   for i = 0:5
+      a = a + bar();
+   end
+end
+
+function [result] = bar()
+   result = 1.0;
+end
+
