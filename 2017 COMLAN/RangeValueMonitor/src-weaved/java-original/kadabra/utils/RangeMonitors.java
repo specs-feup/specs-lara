7059b3e285314c5fdaf422ea828cd8c6813bd7e6
JoaoBispo <joaobispo@gmail.com>
2017-08-02 01:49:12 +0100

    [COMLAN] Removes imports from some original versions, adds original Matisse version of RangeValueMonitor

---
+++ 2017 COMLAN/RangeValueMonitor/src-weaved/java-original/kadabra/utils/RangeMonitors.java
@@ -0,0 +1,30 @@
+package kadabra.utils;
+
+
+public class RangeMonitors {
+    public static double[] monitorMax = new double[ 1 ];
+
+    public static double[] monitorMin = new double[ 1 ];
+
+    static {
+        kadabra.utils.RangeMonitors.monitor_range_init();
+    }
+
+    public static void monitor_range_init() {
+        
+        for(int i=0; i < 1; i++) {
+            
+            kadabra.utils.RangeMonitors.monitorMin[i] = Double.POSITIVE_INFINITY;
+            kadabra.utils.RangeMonitors.monitorMax[i] = Double.NEGATIVE_INFINITY;
+        }
+    }
+
+    public static void monitor_range_update(int id, double value) {
+        if(value < monitorMin[id]) monitorMin[id] = value;
+        if(value > monitorMax[id]) monitorMax[id] = value;
+    }
+
+    public static void monitor_range_print() {
+    }
+}
+
