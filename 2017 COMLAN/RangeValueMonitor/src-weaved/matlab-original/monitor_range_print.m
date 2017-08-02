e436d88430e37569685b864504620384baef555a
JoaoBispo <joaobispo@gmail.com>
2017-08-02 15:44:49 +0100

    [COMLAN] Removes imports from Kadabra and Matisse version of RangeValueMonitor

--- 2017 COMLAN/RangeValueMonitor/src-weaved/matlab-original/monitor_range_print.m
+++ 2017 COMLAN/RangeValueMonitor/src-weaved/matlab-original/monitor_range_print.m
@@ -1,13 +1,13 @@
 function monitor1_print_ranges()
-   log_file_0 = fopen('ranges_foobar.m.txt', 'at+');
    global monitor_range_min monitor_range_max
-   fprintf(log_file_0, 'foo\n');
-   fprintf(log_file_0, '\ta: {%f, %f}\n', monitor_range_min(1), monitor_range_max(1));
-   fprintf(log_file_0, 'bar\n');
-   fprintf(log_file_0, '\tresult: {%f, %f}\n', monitor_range_min(2), monitor_range_max(2));
-   fprintf(log_file_0, 'libcal\n');
-   fprintf(log_file_0, '\tb: {%f, %f}\n', monitor_range_min(3), monitor_range_max(3));
+   log_file = fopen('ranges_foobar.m.txt', 'at+');
+   fprintf(log_file, 'foo\n');
+   fprintf(log_file, '\tb: {%f, %f}\n', monitor_range_min(1), monitor_range_max(1));
+   fprintf(log_file, 'bar\n');
+   fprintf(log_file, '\tb: {%f, %f}\n', monitor_range_min(2), monitor_range_max(2));
+   fprintf(log_file, 'libcal\n');
+   fprintf(log_file, '\tb: {%f, %f}\n', monitor_range_min(3), monitor_range_max(3));
+   fclose(log_file);
    % End of function
-   fclose(log_file_0);
 end
 
