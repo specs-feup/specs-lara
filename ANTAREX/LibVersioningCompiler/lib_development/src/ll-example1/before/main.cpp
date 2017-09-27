99586f57f7d590058191a179a69ada4dfffa5b77
pedro <p.pinto@fe.up.pt>
2017-09-27 19:19:28 +0100

    [LibVersioningCompiler] Adds API for error checking and handling

--- ANTAREX/LibVersioningCompiler/lib_development/src/ll-example1/before/main.cpp
+++ ANTAREX/LibVersioningCompiler/lib_development/src/ll-example1/before/main.cpp
@@ -2,12 +2,7 @@
 
 int main(int argc, char const *argv[]) {
 
-  // some code here before function call
+	kernel();
 
-  // some DSL annotation here - VERSION THIS FUNCTION
-  kernel();
-
-  // some code here after function call
-
-  return 0;
+	return 0;
 }
