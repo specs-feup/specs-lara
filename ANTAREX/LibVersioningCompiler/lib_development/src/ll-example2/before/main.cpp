955fdff44956ff29817ccfdab77127c6cbe869be
pedro <p.pinto@fe.up.pt>
2017-09-29 18:15:35 +0100

    [LibVC] Starts work on example 2

--- ANTAREX/LibVersioningCompiler/lib_development/src/ll-example2/before/main.cpp
+++ ANTAREX/LibVersioningCompiler/lib_development/src/ll-example2/before/main.cpp
@@ -2,12 +2,7 @@
 
 int main(int argc, char const *argv[]) {
 
-  // some code here before function call
-
-  // some DSL annotation here - VERSION THIS FUNCTION
   kernel();
-
-  // some code here after function call
 
   return 0;
 }
