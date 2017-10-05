ee7dc38fee2d25819b5c5bac1538ec9728c97373
pedro <p.pinto@fe.up.pt>
2017-10-05 18:36:11 +0100

    [McAuto] Adds basic testing structure
    This directory is intended for the development of aspects that facilitate the integration of the Monte Carlo simulation code with the mARGOt autotuning library.

---
+++ ANTAREX/McAuto/src/first_test/main.c
@@ -0,0 +1,24 @@
+void version_1() {
+    
+    return;
+}
+
+void version_2() {
+    
+    return;
+}
+
+int main() {
+    
+    int value = 2;
+    
+    if(value > 2) {
+        
+        version_1();
+    }else{
+        
+        version_2();
+    }
+    
+    return 0;
+}
