cefd070f443d77880d067cccf2d5b2d46ff3065f
Pedro Pinto <p.pinto@fe.up.pt>
2018-05-11 17:47:48 +0100

    [AntarexClavaApi] Adds first version of OpenCL LARA library
    This library replaces a call to a function with a call to its
    corresponding OpenCL kernel and all the boilerplate code needed.

--- ANTAREX/AntarexClavaApi/src-java/pt/up/fe/specs/antarex/clava/LaraAntarexApiResource.java
+++ ANTAREX/AntarexClavaApi/src-java/pt/up/fe/specs/antarex/clava/LaraAntarexApiResource.java
@@ -22,6 +22,9 @@ import org.lara.interpreter.weaver.utils.LaraResourceProvider;
 public enum LaraAntarexApiResource implements LaraResourceProvider {
     TEST("_Test.lara"),
 
+    // OpenCL
+    KERNEL_REPLACER("opencl/KernelReplacer.lara"),
+
     // Examon
     EXAMON("examon/Examon.lara"),
     EXAMON_ASPECTS("examon/ExamonAspects.lara"),
